2d234f53c6eea0a94dade1aac23e13c716b2aa18
nayem_mir <mirnayem49@gmail.com>
2020-03-30 21:44:53 +0600

    small changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('role_id')->index();
-            $table->integer('is_active')->default(0);
+            $table->integer('is_active')->default(1);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
